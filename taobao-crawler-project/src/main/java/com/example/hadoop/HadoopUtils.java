c81a79723397fb012becdfa9cc4465fc06b3b19b
zzzzzz <151901074+Pzfswife@users.noreply.github.com>
2025-03-04 23:50:37 +0800

    Update HadoopUtils.java

--- taobao-crawler-project/src/main/java/com/example/hadoop/HadoopUtils.java
+++ taobao-crawler-project/src/main/java/com/example/hadoop/HadoopUtils.java
@@ -1,20 +1,21 @@
-package com.example.hadoop;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-
 import java.io.IOException;
 
 public class HadoopUtils {
-    public static void writeToHDFS(String data, String filePath) throws IOException {
+    public static void saveData(String data, String filePath) {
         Configuration conf = new Configuration();
         conf.set("fs.defaultFS", "hdfs://localhost:9000");
-        FileSystem fs = FileSystem.get(conf);
-        Path path = new Path(filePath);
-        org.apache.hadoop.fs.FSDataOutputStream outputStream = fs.create(path);
-        outputStream.write(data.getBytes());
-        outputStream.close();
-        fs.close();
+        try {
+            FileSystem fs = FileSystem.get(conf);
+            Path path = new Path(filePath);
+            org.apache.hadoop.fs.FSDataOutputStream outputStream = fs.create(path);
+            outputStream.write(data.getBytes());
+            outputStream.close();
+            fs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
